f637f08d0594e490ffbc22258c7ff4540ff76aed
Angular CLI <angular-cli@angular.io>
2018-02-20 09:00:54 +0530

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,10 @@
+import {Hero} from './hero';
+
+export const Hero[]=[
+  {id : 11, name: 'Mr. Rusiru'},
+  {id : 12, name: 'Mrs. Poornima'},
+  {id : 13, name: 'Mrs. Sachini'},
+  {id : 14, name: 'Mr. Randika'},
+  {id : 15, name: 'Mr. Gimhana'},
+  {id : 16, name: 'Mr. Susantha'},
+];
